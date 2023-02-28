80fafcffd0bdfc1c86d51ff485873d010d59c367
Rustam <rustamxoliqov777@gmail.com>
2023-02-28 05:08:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+const age = +prompt("How old are you", "");
+if (age > 25) {
+    console.log("Horror films");
+} else if (age > 18){
+    console.log("hack fims");
+} else {
+    console.log("Carton");
+};
